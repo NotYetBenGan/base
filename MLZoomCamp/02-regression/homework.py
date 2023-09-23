3ff112dcc0436d8b8912097940c17bbab193d1bb
Vladimir Afanasev <56558362+NotYetBenGan@users.noreply.github.com>
2023-09-23 23:20:49 +0200

    Q3 part1.py

--- MLZoomCamp/02-regression/homework.py
+++ MLZoomCamp/02-regression/homework.py
@@ -12,7 +12,6 @@ hsb.columns[hsb.isnull().any()].tolist()
 #Question 2: What's the median (50% percentile) for variable 'population'?
 hsb['population'].median()
 
-
 n = len(hsb)
 n_val = int(n*0.2)
 n_test = int(n*0.2)
@@ -29,5 +28,28 @@ hsb_val = hsb.iloc[idx[n_train:n_train+n_val]]
 hsb_test = hsb.iloc[idx[n_train+n_val:n]]
 
 #Apply the log transformation:
-median_house_value_log = np.log1p(hsb.median_house_value)
-sns.histplot(median_house_value_log)
+y_train = np.log1p(hsb_train.median_house_value)
+y_val = np.log1p(hsb_val.median_house_value)
+y_test = np.log1p(hsb_test.median_house_value)
+
+
+#Question 3:
+def train_linear_regression(X, y):
+    ones = np.ones(X.shape[0])
+    X = np.column_stack([ones, X])
+
+    XTX = X.T.dot(X)
+    XTX_inv = np.linalg.inv(XTX)
+    w_full = XTX_inv.dot(X.T).dot(y)
+    
+    return w_full[0], w_full[1:]
+
+#Replace with 0 and train
+X_train_0 = hsb_train[hsb_train.columns].fillna(0).values
+w0_0, w_0 = train_linear_regression(X_train_0, y_train)
+y_pred_0 = w0_0 + X_train_0.dot(w_0)
+
+#Replace with mean() and train
+X_train_mean = hsb_train[hsb_train.columns].fillna(hsb_train['total_bedrooms'].mean()).values
+w_mean_0, w_mean = train_linear_regression(X_train_mean, y_train)
+y_pred_mean = w_mean_0 + X_train_mean.dot(w_mean)
