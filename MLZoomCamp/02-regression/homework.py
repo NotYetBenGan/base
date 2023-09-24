778926a09c3ff8426a21aaad961aaadc4419dbcc
Vladimir Afanasev <56558362+NotYetBenGan@users.noreply.github.com>
2023-09-24 14:19:06 +0200

    Add Q4

--- MLZoomCamp/02-regression/homework.py
+++ MLZoomCamp/02-regression/homework.py
@@ -6,8 +6,12 @@ housing = pandas.read_csv(r'https://raw.githubusercontent.com/alexeygrigorev/dat
 hsb = housing.loc[housing['ocean_proximity'].isin(['<1H OCEAN','INLAND']) , ['latitude','longitude','housing_median_age','total_rooms','total_bedrooms','population','households','median_income','median_house_value']]
 sns.histplot(hsb.median_house_value)
 
+####################################
+
 #Question 1: There's one feature with missing values. What is it?
 hsb.columns[hsb.isnull().any()].tolist()
+
+####################################
 
 #Question 2: What's the median (50% percentile) for variable 'population'?
 hsb['population'].median()
@@ -44,12 +48,54 @@ def train_linear_regression(X, y):
     
     return w_full[0], w_full[1:]
 
-#Replace with 0 and train
+#Replace with 0 
+#Train dataset
 X_train_0 = hsb_train[hsb_train.columns].fillna(0).values
 w0_0, w_0 = train_linear_regression(X_train_0, y_train)
-y_pred_0 = w0_0 + X_train_0.dot(w_0)
+#Validate dataset (20% of full dataset hsb) 
+X_val_0 = hsb_val[hsb_val.columns].fillna(0).values
+y_pred_0 = w0_0 + X_val_0.dot(w_0)
 
-#Replace with mean() and train
+#Replace with mean()
+#Train dataset
 X_train_mean = hsb_train[hsb_train.columns].fillna(hsb_train['total_bedrooms'].mean()).values
 w_mean_0, w_mean = train_linear_regression(X_train_mean, y_train)
-y_pred_mean = w_mean_0 + X_train_mean.dot(w_mean)
+#Validate dataset (20% of full dataset hsb) 
+X_val_mean = hsb_val[hsb_val.columns].fillna(hsb_val['total_bedrooms'].mean()).values
+y_pred_mean = w_mean_0 + X_val_mean.dot(w_mean)
+
+#RMSE
+def rmse(y, y_pred):
+    se = (y - y_pred) ** 2
+    mse = se.mean()
+    return np.sqrt(mse)
+
+rmse(y_val, y_pred_0).round(2)
+rmse(y_val, y_pred_mean).round(2)
+
+####################################
+
+#Question 4:
+def train_linear_regression_reg(X, y, r):
+    ones = np.ones(X.shape[0])
+    X = np.column_stack([ones, X])
+
+    XTX = X.T.dot(X)
+    XTX = XTX + r * np.eye(XTX.shape[0])
+
+    XTX_inv = np.linalg.inv(XTX)
+    w_full = XTX_inv.dot(X.T).dot(y)
+    
+    return w_full[0], w_full[1:]
+
+#Replace with 0 
+#Train and validate dataset for different r parameter
+for r in [0, 0.000001, 0.0001, 0.001, 0.01, 0.1, 1, 5, 10]:
+    X_train_0 = hsb_train[hsb_train.columns].fillna(0).values
+    w0_0, w_0 = train_linear_regression_reg(X_train_0, y_train, r)
+    
+    X_val_0 = hsb_val[hsb_val.columns].fillna(0).values
+    y_pred_0 = w0_0 + X_val_0.dot(w_0)
+    
+    score = rmse(y_val, y_pred_0).round(2)
+    print(r, w0, score)
