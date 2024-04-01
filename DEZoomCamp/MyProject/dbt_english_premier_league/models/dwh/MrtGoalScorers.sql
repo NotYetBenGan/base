28a58e8b1a55346a4659e0abca1e4f22cb097ba2
vpered.k.uspehu@gmail.com <vpered.k.uspehu@gmail.com>
2024-04-01 20:57:42 +0000

    add new Marts

--- DEZoomCamp/MyProject/dbt_english_premier_league/models/dwh/MrtGoalScorers.sql
+++ DEZoomCamp/MyProject/dbt_english_premier_league/models/dwh/MrtGoalScorers.sql
@@ -4,16 +4,12 @@
     )
 }}
 
-with DimPlayerPerf as (
-SELECT playerId, count(*) as GoalsScored
-FROM {{ ref('DimPlayerPerf') }}
-WHERE 1=1
-  AND typeOfStat IN (1,2) --goal or by penalty
-GROUP BY playerId
-)
-
-SELECT dp.PlayerName, dpp.GoalsScored
-FROM DimPlayerPerf dpp
+SELECT fm.seasonStartYear, dpp.*, dp.playerName
+FROM {{ ref('FactMatch') }} fm 
+INNER JOIN {{ ref('DimPlayerPerf') }} dpp
+  ON fm.matchId = dpp.matchId 
 INNER JOIN {{ ref('DimPlayer') }} dp
   ON dpp.playerId = dp.playerId
-ORDER BY dpp.GoalsScored DESC
+WHERE 1=1
+  AND dpp.typeOfStat IN (1,2) --goal or by penalty
+
